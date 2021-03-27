bbaa66d0ef052cb80bb8d1bf3252627c7b5a3a6f
tvingmark <emmigunn+os@gmail.com>
2021-03-27 23:51:51 +0000

    add tailwindcss with vscode ext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
 
 function App({ Component, pageProps}: AppProps ){
     return <Component {...pageProps}/>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -21,7 +21,7 @@ export default function Post({
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className="text-2xl font-extrabold">{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
