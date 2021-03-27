328f4651648cd0d98d68f1937baf233752c7cb90
tvingmark <emmigunn+os@gmail.com>
2021-03-27 22:11:31 +0000

    ts commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,28 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Believer in a fair world.</p>
+        <blockquote>
+            I will talk about education, tecknology, quantum computing and
+            sustainability.
+        </blockquote>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -38,11 +47,12 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
+export const getStaticProps : GetStaticProps = async () =>
+{
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
     }
-  }
-}
+} 
