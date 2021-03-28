9e93971966a6bda4176627a30722518c9dfa65c5
tvingmark <emmigunn+os@gmail.com>
2021-03-28 15:45:44 +0000

    bg center + isLoading

---
+++ components/map.tsx
@@ -0,0 +1,13 @@
+
+export default function Map({
+    children,
+    home 
+}: {
+ children: React.ReactNode
+ home?: boolean   
+}) {
+  return (
+    <div className="w-full h-14 sm:h-28 bg-purple-400 rounded-md my-2 sm:my-3">
+    </div>
+  )
+}
