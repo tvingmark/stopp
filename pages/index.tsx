48342c7c7cd4fa10eb79020189f5f3b0390ce082
tvingmark <emmigunn+os@gmail.com>
2021-04-14 16:00:35 +0000

    min loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,41 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react' 
-import Layout, { siteTitle } from '../components/layout'
-import Map from '../components/map'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import Layout, { siteTitle } from "../components/layout";
+import Map from "../components/map";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { GetStaticProps } from "next";
 
 export default function Home({
-  allPostsData
+  allPostsData,
 }: {
   allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }) {
-    const [isLoading, setLoading] = useState<boolean>(false);
-    useEffect(() => {
-      let timer = setTimeout(() => setLoading(true), 1 * 300);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    let timer = setTimeout(() => setLoading(true), 1 * 100);
 
-      // this will clear Timeout
-      // when component unmount like in willComponentUnmount
-      // and show will not change to true
-      return () => {
-        clearTimeout(timer);
-      };
-    }, []);
-  if(!isLoading){
-      return (
-          <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-          </Layout>
-      )
+    // this will clear Timeout
+    // when component unmount like in willComponentUnmount
+    // and show will not change to true
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  if (!isLoading) {
+    return (
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+      </Layout>
+    );
   }
   // This is the Coordinator. Home of data. Do you want map or list ?.
   // Create three rectangles around your location that you define according to distance to your home.
@@ -44,18 +44,16 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Map>
-      </Map> 
+      <Map></Map>
     </Layout>
-  )
+  );
 }
 
-export const getStaticProps : GetStaticProps = async () =>
-{
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-} 
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
