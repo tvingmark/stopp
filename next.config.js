7b38c368cad1b8f71c9737bc5ebd7643fe04a05e
tvingmark <emmigunn+os@gmail.com>
2021-04-14 13:42:19 +0000

    webpack nextjs fix for webworker

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ module.exports = withPWA({
   future: {
     webpack5: true,
   },
+  webpack: (config, { isServer, dev }) => {
+    //https://github.com/vercel/next.js/issues/22813#issuecomment-810961712
+    config.output.chunkFilename = isServer
+      ? `${dev ? "[name]" : "[name].[fullhash]"}.js`
+      : `static/chunks/${dev ? "[name]" : "[name].[fullhash]"}.js`;
+
+    return config;
+  },  
   pwa: {
     dest: 'public'
   }
