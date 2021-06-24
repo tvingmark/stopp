4d0e334f71fbcdf2935c7d20fbf038af2b21f173
tvingmark <emmigunn+os@gmail.com>
2021-06-24 13:00:37 +0000

    blue location dot

--- components/maps/customMarker.ts
+++ components/maps/customMarker.ts
@@ -25,12 +25,13 @@ export default function CustomBikeMarker(bike: BikeStatus) {
 export function HomeMarker() {
   var el = document.createElement("div");
   el.className =
-    "marker rounded-full bg-transparent mx-1 z-50 flex justify-center items-center";
-  el.style.width = "30px";
-  el.style.height = "30px";
+    "marker bg-blue-100 bg-opacity-25 rounded-full bg-transparent mx-1 z-50 flex justify-center items-center";
+  el.style.width = "18px";
+  el.style.height = "18px";
   el.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-  <path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/>
+  <svg width="30" height="30" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg"> 
+  <circle cx="28" cy="28" r="25" stroke="white" stroke-width="5"/
+  <circle cx="28" cy="28" r="20" fill="#4285F4"/>
   </svg>
   `;
   return el;
